dd113e63d5541b3b0ab13c1af9941c31adf945d7
meironY2008 <67857621+meironY2008@users.noreply.github.com>
2022-11-09 16:03:34 +0200

    header start

--- client/src/Header/index.tsx
+++ client/src/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./Header.css";
 import api, { baseURL } from "../utils/api"; //axios with necessary configurations
 import { auth } from "../firebase-auth";
@@ -13,17 +13,17 @@ import {
   Modal,
 } from "react-bootstrap";
 // import UsernamePopup from "./UsernamePopup";
-import default_profile from "../default_profile.svg";
-import useDevice from "../Hooks/useDevice";
+// import default_profile from "../default_profile.svg";
+// import useDevice from "../Hooks/useDevice";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../Auth/ContextProvider";
 import SignUp from "../Auth/SignUp";
 import LogIn from "../Auth/LogIn";
 function Header() {
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const [showSignUp, setShowSignUp] = useState(false);
   const [showLogIn, setShowLogIn] = useState(false);
-  const logout =() => {
+  const logout = () => {
     auth.signOut();
   };
 
@@ -42,48 +42,79 @@ function Header() {
     });
     eventSource.onmessage = (e) => {
       if (isJSON(e.data)) {
-        
       }
     };
     return () => {
       eventSource.close();
     };
   }, []);
+
   return (
     <>
       {!(
         window.location.href.includes("/sign-up") ||
         window.location.href.includes("/log-in")
       ) && (
-        <header>
-          <Link id="logo" to="/">Papayask</Link>
-          <div id="search-bar">
-            <Form>
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2"
-                id="search-input"
-              />
-              <Button variant="success" id="search-button">
-                Search
-              </Button>
-            </Form>
+        <header className="header-container">
+          <Link id="logo" to="/">
+            <img src="/assets/images/PapayaLogo.svg" />
+            Papayask
+          </Link>
+
+          <Form id="search-bar">
+            <Button variant="success" id="search-button">
+              <img src={"/assets/images/search.svg"} />
+            </Button>
+            <FormControl
+              type="text"
+              placeholder="Search..."
+              className="mr-sm-2"
+              id="search-input"
+            />
+            <Button variant="success" id="speaker-button">
+              <img src={"/assets/images/speaker.svg"} />
+            </Button>
+          </Form>
+
+          <div className="images-header-container">
+            <img
+              className="header-img"
+              src={"/assets/images/directRequest.svg"}
+            />
+            <img className="header-img" src={"/assets/images/bell.svg"} />
+            <img className="header-img" src={"/assets/images/message.svg"} />
+            <img className="header-img" src={"/assets/images/heart.svg"} />
+            <img className="header-img" src={"/assets/images/user.svg"} />
           </div>
-              {(user)?<Button onClick={logout}>Log Out</Button>:<>
-              <Button onClick={()=>setShowSignUp(true)}>Sign Up</Button><Button onClick={()=>setShowLogIn(true)}>Log In</Button></>}
+
+          {/* {user ? (
+            <Button onClick={logout}>Log Out</Button>
+          ) : (
+            <>
+              <Button onClick={() => setShowSignUp(true)}>Sign Up</Button>
+              <Button onClick={() => setShowLogIn(true)}>Log In</Button>
+            </>
+          )} */}
         </header>
       )}
-      {showSignUp?<Modal
-      show={showSignUp&&!(user)}
-      onHide={() => setShowSignUp(false)}
-      dialogClassName="register-modal"
-    ><SignUp/></Modal>:null}
-      {showLogIn?<Modal
-      show={showLogIn&&!(user)}
-      onHide={() => setShowLogIn(false)}
-      dialogClassName="register-modal"
-    ><LogIn/></Modal>:null}
+      {showSignUp ? (
+        <Modal
+          show={showSignUp && !user}
+          onHide={() => setShowSignUp(false)}
+          dialogClassName="register-modal"
+        >
+          <SignUp />
+        </Modal>
+      ) : null}
+      {showLogIn ? (
+        <Modal
+          show={showLogIn && !user}
+          onHide={() => setShowLogIn(false)}
+          dialogClassName="register-modal"
+        >
+          <LogIn />
+        </Modal>
+      ) : null}
     </>
   );
 }
