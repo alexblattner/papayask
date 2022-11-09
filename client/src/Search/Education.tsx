7dcead042d8c618798e6876e36f802115211692e
randomguy <alexblattnershalom@gmail.com>
2022-11-09 16:39:31 +0200

    save fix

--- client/src/Search/Education.tsx
+++ client/src/Search/Education.tsx
@@ -1,11 +1,23 @@
 import React, { useContext, useState, useEffect } from "react";
 import axios from "axios";
 import Max from "./Max";
+import api from "../utils/api";
+import { UniversityProps } from "../models/University";
 const Education = () => {
     const [rank,setRank]=useState<number>(100);
     const [school,setSchool]=useState<string>("");
     const [degree,setDegree]=useState<string>("");
     const [results,setResults]=useState<string[]>([]);
+    const educationSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        if(value.length>0){
+          const res = await api.get("/university/"+value);
+          setResults(res.data);
+        }else{
+          setResults([]);
+        }
+    };
+
     return (
       <div  className="filter-popup">
         <div>Education</div>
@@ -26,8 +38,8 @@ const Education = () => {
         <div>
           <span>University</span>
           <div>
-            <input type="text" placeholder="Search for university expert studied at"/>
-            {results.map((result:string)=><div>{result}</div>)}
+            <input type="text" onChange={educationSearch} placeholder="Search for university expert studied at"/>
+            {results.map((result:any)=><div>{result.name}</div>)}
           </div>
         </div>
         <div>
---
+++ client/src/models/University.tsx
@@ -0,0 +1,7 @@
+export type UniversityProps = {
+    name: string,
+    rank: number,
+    country: string,
+    createdAt: string | undefined,
+    uptedAt: string | undefined,
+}
