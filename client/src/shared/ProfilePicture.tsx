a5e2ae4a9b79356ff4b71bea9474d10c91becfc4
elia-dot <eliacohen100@gmail.com>
2022-12-20 11:19:22 +0200

    fixed profile image resizing

--- client/src/shared/ProfilePicture.tsx
+++ client/src/shared/ProfilePicture.tsx
@@ -42,7 +42,7 @@ const ProfilePicture = (props: Props) => {
     } else {
       const img = cld.image(`${process.env.REACT_APP_ENV!}/${props.src}`);
       img
-        .resize(fill(props.size || 100, props.size || 100))
+        .resize(fill(props.size || 100, props.size || 100).gravity('faces'))
         .roundCorners(byRadius(props.radius || 0));
       setImage(img);
     }
