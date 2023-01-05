8784918b71a04a66bbfdff7919eb7dc85f754378
meironY2008 <67857621+meironY2008@users.noreply.github.com>
2023-01-05 10:38:34 +0200

    Update SvgIcon.tsx

--- client/src/shared/SvgIcon.tsx
+++ client/src/shared/SvgIcon.tsx
@@ -1,8 +1,8 @@
-import Icons from '../data/sprite.svg';
+import Icons from "../data/sprite.svg";
 
 interface IconProps {
   src: string;
- size?: number;
+  size?: number;
   color?: string;
 }
 
@@ -10,15 +10,13 @@ const Icon = ({ src, size, color }: IconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width={size ? size : '24'}
-      height={size ? size : '24'}
-      fill={color ? color : 'currentColor'}
+      width={size ? size : "24"}
+      height={size ? size : "24"}
+      fill={color ? color : "currentColor"}
     >
-      <use href={`${Icons}#${src}`} fill={color ? color : 'currentColor'} />
+      <use href={`${Icons}#${src}`} fill={color ? color : "currentColor"} />
     </svg>
   );
 };
 
 export default Icon;
-
-
